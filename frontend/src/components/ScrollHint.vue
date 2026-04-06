ae6e9e0b5e201d566dfb9f70a0632a6b5533cd4b
y-nagai0725 <y.nagai0725@gmail.com>
2026-04-07 01:21:09 +0900

    コメント追加など

--- frontend/src/components/ScrollHint.vue
+++ frontend/src/components/ScrollHint.vue
@@ -8,8 +8,14 @@
 </template>
 
 <script setup>
+// =========================================================================
+// パッケージ・モジュールの読み込み
+// =========================================================================
 import ArrowIcon from "@/components/icons/ArrowIcon.vue";
 
+// =========================================================================
+// Props & Emits
+// =========================================================================
 defineProps({
   show: {
     type: Boolean,
@@ -23,30 +29,33 @@ defineProps({
   position: absolute;
   top: 50%;
   right: 1.6rem;
+  display: flex;
+  gap: 0.8rem;
+  align-items: center;
+  padding: 0.8rem 1.6rem;
+
+  /* ヒントの上を指でスワイプしても、
+     後ろにある要素がスクロールするように操作を透過させるため */
+  pointer-events: none;
+
+  /* 背景の画像や文字と同化して見えなくなるのを防ぐために、少し透ける黒を敷く */
+  background-color: rgba($black, 0.65);
+  border-radius: 100vmax;
+  box-shadow: 0 2px 8px rgba($black, 0.15);
   transform: translateY(-50%);
 
-  display: flex;
-  align-items: center;
-  gap: 0.8rem;
-
-  padding: 0.8rem 1.6rem;
-  border-radius: 100vmax;
-  background-color: rgba($black, 0.65); // 半透明の黒背景
-  box-shadow: 0 2px 8px rgba($black, 0.15); // 少しだけ影をつける
-
-  pointer-events: none;
-  z-index: 5;
-
+  /* PCでは横スクロールを発生させないデザインなので、ヒント自体を隠す */
   @include pc {
     display: none;
   }
 
   &__text {
-    font-weight: $bold;
     @include fluid-text(12, 14);
+
+    font-weight: $bold;
+    line-height: 1;
     color: $white;
     letter-spacing: 0.05em;
-    line-height: 1;
   }
 
   &__icon {
@@ -55,12 +64,16 @@ defineProps({
   }
 }
 
+/* =======================================================================
+ * トランジション（アニメーション）の設定
+ * ======================================================================= */
+
 .fade-enter-active,
 .fade-leave-active {
   transition: opacity $transition-base, transform $transition-base;
 }
 
-// 少し右からフワッとスライドしてくるアニメーション
+/* 少し右側からフワッとスライドしてくるアニメーション。 */
 .fade-enter-from,
 .fade-leave-to {
   opacity: 0;
