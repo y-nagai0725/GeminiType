9d233858a55fd0307cdedb1099966d06e60f1eb6
y-nagai0725 <y.nagai0725@gmail.com>
2026-04-07 00:27:23 +0900

    コメント追加

--- frontend/src/components/Loading.vue
+++ frontend/src/components/Loading.vue
@@ -3,40 +3,51 @@
     {{ text }}
   </div>
 </template>
+
 <script setup>
-/**
- * Props定義
- */
 const props = defineProps({
-  // 表示テキスト（デフォルトは空文字）
+  // 何の処理を待っているかのテキスト
   text: { type: String, default: "" },
-  // 背景色（デフォルトは灰色）
+  // スピナーの軌道（ベースの円）の色
   bgColor: { type: String, default: "gray" },
-  // 線の色（デフォルトは緑）
+  // くるくる回る線の色
   lineColor: { type: String, default: "green" },
 });
 </script>
 
 <style lang="scss" scoped>
 .loading {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
   @include fluid-style(gap, 10, 16);
   @include fluid-text(12, 16);
 
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  /* ローディングのくるくる回る円（スピナー）本体 */
   &::after {
-    content: "";
     width: 2em;
     aspect-ratio: 1;
-    border-radius: 100vmax;
-    border-width: 4px;
-    border-style: solid;
+    content: "";
+
+    /* 全体の円（軌道）の色。 */
     border-color: $gray;
+    border-style: solid;
+    border-width: 4px;
+
+    /* 上の1辺だけ色を変えることで、この色の部分がくるくる回るように見せる */
     border-top-color: $green;
+
+    /* どんなサイズに変わっても絶対に真ん丸な円を保つための指定 */
+    border-radius: 100vmax;
+
+    /* linearを指定することで、カクカクせず一定の速度で滑らかに回り続けるようにする */
     animation: spin 1s linear infinite;
   }
 
+  /* =======================================================================
+   * Propsで渡された色に応じたカラーバリエーション
+   * ======================================================================= */
   &.bg-white::after {
     border-color: $white;
   }
